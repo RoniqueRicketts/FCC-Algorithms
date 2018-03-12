e1b4421fdc1e52a7eadc2c750d78b7b2ba234181
Ronique Ricketts <johnoyrick@gmail.com>
2018-03-11 21:39:10 -0500

    updated Check for Palindromes

--- check_for_palindromes.js
+++ check_for_palindromes.js
@@ -2,3 +2,11 @@ function palindrome(str) {
 return str.replace(/[\W_]/g, '').toLowerCase() === str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
 }
 palindrome("eye");
+
+// I learnt how to do palindrome
+function palindrome(str) {
+  let regexp = /[A-Za-z0-9]/g;
+  let matchedStr = str.match(regexp);
+  let string = matchedStr.join('');
+  return string.toLowerCase() === matchedStr.reverse().join('').toLowerCase();
+palindrome("eye");
